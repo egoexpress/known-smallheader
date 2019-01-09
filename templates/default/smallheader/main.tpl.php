4302effd249665a1a599a2f38914ed368559142a
Björn Stierand <bjoern-github@innovention.de>
2019-01-09 20:19:15 +0100

    fixed fa icon, added styling

--- templates/default/smallheader/main.tpl.php
+++ templates/default/smallheader/main.tpl.php
@@ -7,14 +7,14 @@ if (\Idno\Core\Idno::site()->session()->isLoggedIn()) {
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" 
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
 	            data-target="#navbar-collapse">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" tabindex="1" 
+            <a class="navbar-brand" tabindex="1"
                href="<?= \Idno\Core\Idno::site()->config()->getDisplayURL() ?>"><?=
                  $this->draw('shell/toolbar/title')?>
             </a>
@@ -42,24 +42,28 @@ if (\Idno\Core\Idno::site()->session()->isLoggedIn()) {
 } else {
 ?>
 
+<style>
+    .navbar-fixed-top {
+        display: flex;
+        justify-content: space-between;
+    }
+    .navbar-fixed-top a {
+        display: inline-block;
+        padding: 1em;
+    }
+</style>
+
 <!-- alternate header when not logged in -->
 <nav class="navbar-fixed-top">
-    <table border="0" width="100%">
-		<tr>
-			<td align="left">
-	    		&nbsp;&nbsp;
-        		<a href="/" style="color: #000000">
-	    			<i class="fa fa-desktop"></i>
-				</a>
-			</td>
-			<td align="right">
-        		<a href="/session/login" style="color: #000000">
-	    		<i class="fa fa-sign-in"></i>
-	    		&nbsp;&nbsp;
-			</td>
-		</tr>
-    </table>
+
+    <a href="/" style="color: #000000" title="Go to main page">
+        <i class="fas fa-desktop"></i>
+    </a>
+    <a href="/session/login" style="color: #000000" title="Login to site">
+        <i class="fas fa-sign-in-alt"></i>
+    </a>
 </nav>
+
 <?php
 }
 ?>
